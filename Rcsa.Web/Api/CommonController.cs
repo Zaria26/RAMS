fcf491d39785661b4b5195b96055c7a7334a9528
Zaria26 <135379192+Zaria26@users.noreply.github.com>
2024-07-26 13:32:03 -0400

    Add files via upload

---
+++ Rcsa.Web/Api/CommonController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Rcsa.Web.Models;
+
+namespace Rcsa.Web.Api
+{
+    public class CommonController : ApiController
+    {
+        // GET api/comman
+        public List<int> GetDepartmentsByUserId(int userId, int companyId)
+        {
+            RcsaDb db = new RcsaDb();
+            var list = from x in db.UserDepartments
+                       where x.UserId == userId && x.CompanyId == companyId
+                       select x.DepartmentId;
+            return list.ToList();
+        }
+
+
+        // GET api/comman/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/comman
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/comman/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/comman/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Rcsa.Web/App_Start/DataBaseConfig.cs
@@ -0,0 +1,13 @@
+﻿using WebMatrix.WebData;
+
+namespace Rcsa.Web.App_Start
+{
+  public class DataBaseConfig
+  {
+    public static void RegisterDatabaseConnection()
+    {
+      //TODO : Move to register area
+      WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+    }
+  }
+}
